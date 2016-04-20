270cb0b6d8730e5470fb31f92bfd5e1d7f051cce
Stanisrawr <stlesk@GMAIL.COM>
2016-04-20 02:38:18 -0400

    We have MICE!
    Mouse control,possible android control and some other changes away from
    vive and closer to Cardboard functionality

--- Assets/scripts/Shooty.cs
+++ Assets/scripts/Shooty.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/*
+
 public class Shooty : MonoBehaviour {
 
 	// Use this for initialization
@@ -10,31 +10,70 @@ public class Shooty : MonoBehaviour {
 	}
 
 	public GameObject bulletHole;
+	public AudioSource audioP;
+	public GameObject[] kawaii;
+	public AudioClip pew;
+	public AudioClip[] motivation;
+	RaycastHit hit;
 	// Update is called once per frame
 	void Update () 
 	{		
-		//device = 
-		if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+		/*int nbTouches = Input.touchCount;
 
-			{
-			RaycastHit hit;
+
+		for (int i=0;i<nbTouches;i++)
+		{
+			Touch touch = Input.GetTouch (i);
 			Ray ray = new Ray (transform.position, transform.forward);
-			if (Physics.Raycast (ray, out hit, 100f)) {
-			
+			audioP.clip = pew;
+			audioP.Play();
+			if (Physics.Raycast (ray, out hit, 1000f)) {
+				if (hit.collider.tag == "Enemy") {
+					int k = Random.Range (0, 5);
+					Instantiate (kawaii[k],hit.point, Quaternion.identity);
+					Destroy (hit.collider.gameObject);
+					int j=Random.Range(0,19);
+					audioP.clip = motivation[j];
+					audioP.Play();
+				}
+
+
 				Instantiate (bulletHole, hit.point, Quaternion.FromToRotation (Vector3.up, hit.normal));
-			}
 
 			}
 
+			}
+
+*/
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = new Ray (transform.position, transform.forward);
+			audioP.clip = pew;
+			audioP.PlayOneShot (pew, 03f);
+			if (Physics.Raycast (ray, out hit, 1000f)) {
+				Instantiate (bulletHole, hit.point, Quaternion.FromToRotation (Vector3.up, hit.normal));
+
+				if (hit.collider.tag == "Enemy") {
+					int k = Random.Range (0, 5);
+					Instantiate (kawaii[k],hit.point, Quaternion.identity);
+					Destroy (hit.collider.gameObject);
+					int j=Random.Range(0,19);
+					audioP.clip = motivation[j];
+					audioP.Play();
+				}
 
 
+
+			}
+
+		
+		}
 
 	}
 
 }
 
 
-*/
+
 	
 					
 
